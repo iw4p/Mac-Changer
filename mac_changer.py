f3d8d47db09827483e90317f08f7b1c22315ef64
iw4p <nimakbarzade@yahoo.com>
2019-07-10 01:03:04 -0400

    change mac

--- mac_changer.py
+++ mac_changer.py
@@ -5,7 +5,11 @@ import subprocess
 subprocess.call("ifconfig", shell=True)
 
 subprocess.call("ifconfig eth0 down", shell=True)
-subprocess.call("ifconfig eth0 hw ether 08:00:27:89:03:db", shell=True)
+
+#subprocess.call("ifconfig eth0 hw ether 08:00:27:89:03:db", shell=True)
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+
 subprocess.call("ifconfig eth0 up", shell=True)
 
 subprocess.call("ifconfig", shell=True)
+
