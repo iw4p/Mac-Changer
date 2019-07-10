67a43f336e234967d94b70379a56bef1a8df1847
iw4p <nimakbarzade@yahoo.com>
2019-07-10 01:20:14 -0400

    add input

--- mac_changer.py
+++ mac_changer.py
@@ -2,14 +2,14 @@
 
 import subprocess
 
-subprocess.call("ifconfig", shell=True)
+interface = input("Interface: ")
+new_mac = input("new MAC: ")
 
-subprocess.call("ifconfig eth0 down", shell=True)
+print(" [+] Changing MAC Address for " + interface + " to " + new_mac)
 
-#subprocess.call("ifconfig eth0 hw ether 08:00:27:89:03:db", shell=True)
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:55", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
+#subprocess.call("ifconfig " + interface + " hw ether 08:00:27:89:03:db", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac , shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
 
-subprocess.call("ifconfig eth0 up", shell=True)
-
-subprocess.call("ifconfig", shell=True)
 
